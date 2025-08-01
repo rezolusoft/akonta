9e05a813de89c806912e057eb422cee48f37a5f1
baba-mandef <contact@abiodoun.dev>
2025-08-01 19:53:33 +0100

    📝 update side menu

--- src/components/layout/side_menu.py
+++ src/components/layout/side_menu.py
@@ -1,6 +1,17 @@
 import flet as ft
-from .side_menu_button import side_menu_button
+from .partial.side_menu_button import side_menu_button
 
+
+def menu_title(_title:str)->ft.Control:
+
+    title = ft.Container(ft.Text(value=_title))
+    title.margin = ft.margin.only(top=10)
+    return title
+
+
+spacer = ft.Container()
+
+spacer.margin = ft.margin.symmetric(vertical=10)
 
 side_menu = ft.Container(
     bgcolor=ft.Colors.SURFACE,
@@ -9,12 +20,16 @@ side_menu = ft.Container(
     content=ft.Column(
         expand_loose=True,
         controls=[
-
-            side_menu_button(title="Tableau de Board", icon="DASHBOARD_ROUNDED"),
+            menu_title('Menu Principal'),
+            side_menu_button(title="Dashboard", icon="DASHBOARD_ROUNDED"),
             side_menu_button(title="Mon Stock", icon="WAREHOUSE_ROUNDED"),
             side_menu_button(title="Mes Produits", icon="INVENTORY_ROUNDED"),
             side_menu_button(title="Ma Caisse", icon="WALLET_ROUNDED"),
             side_menu_button(title="Mes statistiques", icon="BAR_CHART_ROUNDED"),
+            spacer,
+            menu_title("Centre d'aide"),
+            side_menu_button(title="Donner un avis", icon="MODE_COMMENT"),
+            side_menu_button(title="Support Client", icon="SUPPORT_AGENT_ROUNDED"),
            
         ],
         
