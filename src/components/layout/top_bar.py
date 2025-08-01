5d3e405eaaea42506fe76a8aca7a9178d29ff4ae
baba-mandef <contact@abiodoun.dev>
2025-08-01 12:01:08 +0100

    ✏️ add logo and divider to topbar

--- src/components/layout/top_bar.py
+++ src/components/layout/top_bar.py
@@ -1,8 +1,25 @@
 import flet as ft
+
+
+logo = ft.Container(ft.Image(src="Logo.png"))
+logo.padding = ft.padding.all(10)
+logo.margin = ft.margin.only(left=10, right=5)
+
+
+divider = ft.Container(ft.VerticalDivider(color=ft.Colors.ON_SURFACE))
+divider.padding = ft.padding.symmetric(vertical=5)
+
 top_bar = ft.Container(
-                        content=ft.Text("Barre d'action horizontale"), 
-                        bgcolor=ft.Colors.WHITE, 
+                        bgcolor=ft.Colors.SURFACE, 
                         expand=True,
-                        height=60)
+                        height=70,
+                        content=ft.Row(controls=[
+                            logo,
+                            divider
+                        ]), 
+                        
+                        )
 
 top_bar.border_radius = ft.border_radius.all(10)
+
+
