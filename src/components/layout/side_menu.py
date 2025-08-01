66b059dfe3137e55e8166cdc5c603152e325ae94
baba-mandef <contact@abiodoun.dev>
2025-08-01 12:01:51 +0100

    📝 update side menu component

--- src/components/layout/side_menu.py
+++ src/components/layout/side_menu.py
@@ -3,7 +3,7 @@ from .side_menu_button import side_menu_button
 
 
 side_menu = ft.Container(
-    bgcolor=ft.Colors.WHITE,
+    bgcolor=ft.Colors.SURFACE,
     expand_loose=True,
     
     content=ft.Column(
@@ -14,7 +14,7 @@ side_menu = ft.Container(
             side_menu_button(title="Mon Stock", icon="WAREHOUSE_ROUNDED"),
             side_menu_button(title="Mes Produits", icon="INVENTORY_ROUNDED"),
             side_menu_button(title="Ma Caisse", icon="WALLET_ROUNDED"),
-            side_menu_button(title="Mes statistiques", icon="BAR_CHART_ROUNDED")
+            side_menu_button(title="Mes statistiques", icon="BAR_CHART_ROUNDED"),
            
         ],
         
@@ -25,5 +25,5 @@ side_menu = ft.Container(
 
 
 side_menu.border_radius = ft.border_radius.all(10)
-side_menu.padding = ft.padding.all(20)
+side_menu.padding = ft.padding.all(10)
 
