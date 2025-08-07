b7eef64838476ef73bec293f2d4c82fb45b76db4
Abiodoun Paraïso <contact@abiodoun.dev>
2025-08-07 12:29:52 +0100

    ✏️ update layout

--- src/main.py
+++ src/main.py
@@ -7,13 +7,12 @@ from pages import dashboard
 
 def main(page: ft.Page):
 
+
+    page.window.maximized = True
     page.title = "Akonta"
-
     page.theme_mode = ft.ThemeMode.LIGHT
-
     page.theme = light_theme
     page.dark_theme = dark_theme
-
     page.bgcolor = page.theme.color_scheme.background
 
     page.add(
@@ -21,11 +20,26 @@ def main(page: ft.Page):
             content = ft.Column(
                 expand=True,
                 controls=[
-                    ft.Row(controls=[top_bar]),
-                    ft.Row(expand=True, controls=[side_menu, dashboard])
+                    ft.Row(expand=True,
+                        controls=[
+                        
+                        side_menu,
+                        ft.Column(
+                            controls=[
+                                top_bar,
+                                 dashboard
+                            ],
+                            expand=True
+                            
+                            ),
+                        
+
+                       ])
                 ],
             ),
             expand=True
         )
     )
+
+
 ft.app(main)
