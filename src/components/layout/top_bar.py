8a2e39277cc5d4fcac08f350fbe6b85863d91d21
Abiodoun Paraïso <contact@abiodoun.dev>
2025-08-07 12:31:35 +0100

    ✏️ update top_bar

--- src/components/layout/top_bar.py
+++ src/components/layout/top_bar.py
@@ -1,30 +1,22 @@
 import flet as ft
-from .partial.top_bar_title import top_bar_title
-
-
-logo = ft.Container(ft.Image(src="Logo.png"))
-logo.padding = ft.padding.all(15)
-logo.margin = ft.margin.only(left=10, right=10)
-
+from .partial import top_bar_title, search_bar, settings_menu
 
 
 top_bar = ft.Container(
-                        bgcolor=ft.Colors.SURFACE, 
-                        expand=True,
-                        height=70,
-                        content=ft.Row(controls=[
-                            ft.Row(
-                                controls=[
-                                    logo,
-                                    top_bar_title('Dashboard', 'Bienvenue, Rezolusoft')
-                                ]
-                            ),
-                            
-                            
-                        ]), 
-                        
-                        )
+    bgcolor=ft.Colors.SURFACE,
 
-top_bar.border_radius = ft.border_radius.all(10)
+    content=ft.Row(controls=[
+        
+        top_bar_title('Dashboard', 'Bon retour sur Akonta'),
+        
+        search_bar(), 
 
+        settings_menu()
 
+    ],
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+    )
+
+)
+
+top_bar.border_radius = ft.border_radius.all(7)
