83ed1c5bd207d6ab42f306da6ab14caae0d099c1
Abiodoun Paraïso <contact@abiodoun.dev>
2025-08-18 00:00:13 +0100

    📝 update shop models

--- src/models/shop.py
+++ src/models/shop.py
@@ -8,6 +8,7 @@ class AkontaShop(AkontaObject):
     email = CharField(max_length=50)
     phone = CharField(max_length=20)
     adress = CharField(max_length=150, null=True)
+    balance = DecimalField(decimal_places=2, default=0)
     ifu = CharField(max_length=100, null=True)
     rccm = CharField(max_length=100, null=True)
     manager = CharField(max_length=150)
