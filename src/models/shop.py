a43978b7d1a7cdd5eb76e2f033bf77e91f3980d4
Abiodoun Paraïso <contact@abiodoun.dev>
2025-08-17 23:56:21 +0100

    📝 add shop model

---
+++ src/models/shop.py
@@ -0,0 +1,14 @@
+from peewee import *
+from models.akonta import AkontaObject
+
+
+class AkontaShop(AkontaObject):
+    name = CharField(max_length=50)
+    logo = CharField(max_length=500, null=True)
+    email = CharField(max_length=50)
+    phone = CharField(max_length=20)
+    adress = CharField(max_length=150, null=True)
+    ifu = CharField(max_length=100, null=True)
+    rccm = CharField(max_length=100, null=True)
+    manager = CharField(max_length=150)
+
