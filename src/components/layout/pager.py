7e3896e2406012de93f7e8576e934339fdf90c0c
Abiodoun Paraïso <contact@abiodoun.dev>
2025-08-30 22:23:52 +0100

    ✏️ try and remove glassmorphism ui

--- src/components/layout/pager.py
+++ src/components/layout/pager.py
@@ -1,32 +1,35 @@
 import flet as ft
-from pages import dashboard
-from components import side_menu, top_bar
+from components.layout import side_menu, top_bar
 
 
 def pager(page, content)->ft.Control:
 
 
-    pager = ft.SafeArea(
-            content = ft.Column(
-                expand=True,
-                controls=[
-                    ft.Row(expand=True,
-                        controls=[
-                        
-                        side_menu(page=page),
-                        ft.Column(
+    pager = ft.Container(
+                content=ft.Column(
+                    expand=True,
+                    controls=[
+                        ft.Row(expand=True,
                             controls=[
-                                top_bar,
-                                ft.Container(content=content, expand=True)
-                            ],
-                            expand=True
                             
-                            ),
-                       ])
-                ],
-            ),
-            expand=True
-        )
+                            side_menu(page=page),
+                            ft.Column(
+                                controls=[
+                                    top_bar(),
+                                    ft.Container(content=content, expand=True)
+                                ],
+                                expand=True
+                                
+                                ),
+                           ])
+                    ],
+                ),
+                # image=ft.DecorationImage("img/3.jpg", fit=ft.ImageFit.COVER),
+                expand=True,
+                border_radius=ft.border_radius.all(5),
+                padding=ft.padding.all(5),
+                # bgcolor=page.theme.color_scheme.background
+            )
     return pager
 
 
