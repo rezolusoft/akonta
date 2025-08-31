c6154286eebcfed27cbe2ab20dde67250eb56f9c
Abiodoun Paraïso <contact@abiodoun.dev>
2025-08-31 10:48:33 +0100

    ✏️ add seller id to sale operation

--- src/models/sale.py
+++ src/models/sale.py
@@ -1,12 +1,15 @@
 from models.akonta import AkontaObject
 from models.product import AkontaProduct
-from extras.tools import sale_code_generator
+from extras.tools import sale_code_generator, id_generator
+from models.user import AkontaUser
 from peewee import *
 
 
 class AkontaSale(AkontaObject):
     amount = DecimalField(decimal_places=2)
+    sale_id = UUIDField(default=id_generator)
     code = CharField(default=sale_code_generator)
+    seller = ForeignKeyField(AkontaUser, backref="sales")
 
     def __str__(self):
         return self.code
